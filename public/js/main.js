866f9d4a3adbdab535452d7ee7f631b0ae1e8446
Bryan Guillermo Arévalo <bryantello2010@hotmail.com>
2020-02-18 13:38:24 -0600

    Agregado el modo oscuro de una manera más optimizada

--- public/js/main.js
+++ public/js/main.js
@@ -1,105 +1,53 @@
-'use strict'
-var openModal = document.querySelector('.btnColor');
-var whiteColor = document.querySelectorAll('.bgWhite');
-var newsContainer = document.querySelector('.newsContainerColor');
-var titleNew = document.querySelectorAll('#titleCard');
-var contentCard = document.querySelectorAll('#contentCard');
-var cardImage = document.querySelectorAll('#cardImage');
-var container = document.querySelector('.container');
+// Dark n ligth mode
+var btn = document.querySelector('.btnColor');
+var bg = document.querySelectorAll('#backgroundColor');
+var text = document.querySelectorAll('#text');
 
-// Making localStorage object
 if (localStorage.theme == null) {
 	localStorage.setItem('theme', 'white');
 }
 
-function ColorSchemaDark() {
-		// Body background
+function darkMode(){
 	document.body.style.background = 'var(--black)';
-	for (var i = 0; i < whiteColor.length; i++) {
-		whiteColor[i].classList.remove("bgWhite");
-		whiteColor[i].classList.add('bgDark');
+	for(var i = 0; i < bg.length; i++){
+		bg[i].classList.remove('bgWhite');
+		bg[i].classList.add('bgDark');
 	}
-	// Nav
-	
-	document.querySelector('#navLogo').style.color = '#fff';
-	document.querySelector('.start').style.color = '#fff';
-	document.querySelector('.news').style.color = '#fff';
-	// Welcome message
-	container.style.border = '2px solid #fff';
-	
-	// News dark mode
-	newsContainer.classList.remove('bgWhite');
-	newsContainer.classList.add('bgDark');
-	for(var i = 0; i < titleNew.length; i ++){
-		titleNew[i].style.color = '#fff';
-		contentCard[i].style.color = '#fff';
-		cardImage[i].style.border = '2px solid #fff';
+	for(var i = 0; i < text.length; i++){
+		text[i].classList.remove('blackText');
+		text[i].classList.add('whiteText');
 	}
-	
-	// Footer
-	document.querySelector('#footer').style.background = 'var(--grey)';
-
 }
 
-function ColorSchemaWhite() {
-	// Body background
+function whiteMode(){
 	document.body.style.background = 'var(--greyBody)';
-	for (var i = 0; i < whiteColor.length; i++) {
-		whiteColor[i].classList.add('bgWhite');
-		whiteColor[i].classList.remove("bgDark");
+	for(var i = 0; i < bg.length; i++){
+		bg[i].classList.remove('bgDark');
+		bg[i].classList.add('bgWhite');
+		
 	}
-	// Nav
-	document.querySelector('#navLogo').style.color = '#000';
-	document.querySelector('.start').style.color = 'var(--blue)';
-	document.querySelector('.news').style.color = 'var(--blue)';
-	// Welcome message
-	container.style.border = '2px solid transparent';
-	// News ligth mode
-	newsContainer.classList.add('bgWhite');
-	newsContainer.classList.remove('bgDark');
-	for(var i = 0; i < titleNew.length; i++){
-		titleNew[i].style.color = '#000';
-		contentCard[i].style.color = '#000';
-		cardImage[i].style.border = 'none';
+	for(var i = 0; i < text.length; i++){
+		text[i].classList.add('blackText');
+		text[i].classList.remove('whiteText');
 	}
-	
-	// Footer
-	document.querySelector('#footer').style.background = 'var(--blue)';
-
 }
 
-// Making theme
-
-if (localStorage.theme == 'dark') {
-	ColorSchemaDark();
-}else{
-	ColorSchemaWhite();
-}
-
-openModal.addEventListener('click', () => {
-
+btn.addEventListener('click', () => {
 	if(localStorage.theme == 'white'){
-		ColorSchemaDark();
-		localStorage.theme = 'dark'
-		console.log(localStorage.theme);
+		darkMode();
+		console.log('if');
+		localStorage.theme = 'dark';
 	}else{
-		ColorSchemaWhite();
+		whiteMode();
+		console.log('else');
 		localStorage.theme = 'white';
-		console.log(localStorage.theme);
-
 	}
-
 });
-
-
-
-
-
-
-
-
-
-
+if (localStorage.theme == 'dark') {
+	darkMode();
+}else{
+	whiteMode();
+}
 
 
 
